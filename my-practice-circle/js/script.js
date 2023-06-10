7056f5b954cf48b70571a7ea703bbbb71e133d39
Reyhane Rahimian <105440791+reyhan586@users.noreply.github.com>
2023-06-11 00:09:59 +0330

    Update script.js

--- my-practice-circle/js/script.js
+++ my-practice-circle/js/script.js
@@ -3,8 +3,8 @@ circle.addEventListener('click' , func);
 container.querySelector('container');
 
 function func (){
-    var x = Math.floor(Math.random() * window.innerWidth);
-    var y = Math.floor(Math.random() * window.innerHeight);
+    var x = Math.floor(Math.random() * 800);
+    var y = Math.floor(Math.random() * 700);
     circle.style.left = x + "px";
     circle.style.top = y + "px";
 }
